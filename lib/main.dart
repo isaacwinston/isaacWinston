d838a319a3ed42d5c87edec33839275cbc903823
isaacwinston <139127759+isaacwinston@users.noreply.github.com>
2023-11-03 09:59:24 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
 
-void main() {
-  runApp(mainPage());
-}
+void main() => runApp(BMICalculator());
 
-class mainPage extends StatelessWidget {
-  const mainPage({super.key});
-
+class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('image/downloadOne.jpg'),
-              ),
-              Text(
-                'Isaac winston',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text('+234 814 2206 440',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ),
-              ),
-
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text('isaacwinston@email.com',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 15.0,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
+      home: InputPage(),
     );
   }
 }
-
